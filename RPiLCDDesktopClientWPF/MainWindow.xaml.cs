7917edc9beabc828429d8e4f466f3965955ad1b8
TNewis <47041450+TNewis@users.noreply.github.com>
2020-03-09 23:25:14 +0000

    WPF Client that can minimise to tray
    WPF version of the PC client starts minimised to system tray

--- RPiLCDDesktopClientWPF/MainWindow.xaml.cs
+++ RPiLCDDesktopClientWPF/MainWindow.xaml.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +22,50 @@ namespace RPiLCDDesktopClientWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NotifyIcon _notifyIcon;
         public MainWindow()
         {
             InitializeComponent();
+
+            _notifyIcon = new NotifyIcon
+            {
+                Icon = new System.Drawing.Icon("./Assets/avalonia-logo.ico"),
+                Visible = true,
+                BalloonTipText = "RPiLCD Client is now running in system tray.",
+                BalloonTipTitle = "RPiLCD Client Minimised",
+                BalloonTipIcon = ToolTipIcon.Info
+            };
+
+            _notifyIcon.MouseDoubleClick +=new System.Windows.Forms.MouseEventHandler(NotifyIcon_MouseDoubleClick);
+            this.StateChanged += new EventHandler(Window_StateChanged);
+
+            this.ShowInTaskbar = true;
+            this.WindowState = WindowState.Minimized;
+            this.Visibility = Visibility.Hidden;
+
+            this.ShowInTaskbar = false;
+        }
+
+        void NotifyIcon_MouseDoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            this.Visibility = Visibility.Visible;
+            this.WindowState = WindowState.Normal;
+        }
+
+        public void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Minimized)
+            {
+                _notifyIcon.Visible = true;
+                this.ShowInTaskbar = false;
+                this.Visibility = Visibility.Hidden;
+            }
+            else if (this.WindowState == WindowState.Normal)
+            {
+                _notifyIcon.Visible = false;
+                this.ShowInTaskbar = true;
+                this.Visibility = Visibility.Visible;
+            }
         }
     }
 }
---
+++ RPiLCDDesktopClientWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,106 @@
+﻿using RPiLCDClientConsole;
+using RPiLCDClientConsole.Services;
+using RPiLCDClientConsole.Update;
+using RPiLCDShared.Services;
+using System.Threading.Tasks;
+
+namespace RPiLCDDesktopClientWPF.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private static LogToEventService _loggingService;
+
+        private static PCClient _client;
+        private static string _message;
+        private static IUpdateBuilder _updateBuilder;
+
+
+        private string _loggedMessage;
+        public string LoggedMessage
+        {
+            get { return _loggedMessage; }
+            set { SetProperty(ref _loggedMessage, value); }
+        }
+
+        private string _loggedWarning;
+        public string LoggedWarning
+        {
+            get { return _loggedWarning; }
+            set { SetProperty(ref _loggedWarning, value); }
+        }
+
+        private string _loggedError;
+        public string LoggedError
+        {
+            get { return _loggedError; }
+            set { SetProperty(ref _loggedError, value); }
+        }
+        public MainWindowViewModel()
+        {
+            _loggingService = new LogToEventService();
+            PCClient.SetLoggingService(_loggingService);
+            _loggingService.MessageLoggedRecievedEvent += ShowLoggedMessageEvent;
+
+            _loggingService.LogMessage("Starting Client...");
+
+            System.Timers.Timer _timer;
+
+            _client = new PCClient();
+            _updateBuilder = new UpdateBuilder();
+
+            AddAllServices(_updateBuilder);
+
+            _timer = new System.Timers.Timer(1000) { AutoReset = true };
+            _timer.Elapsed += async (sender, e) => await Update();
+            _timer.Start();
+        }
+
+        private static Task Update()
+        {
+            return Task.Run(() =>
+            {
+                _message = _updateBuilder.BuildFullUpdateString();
+                _client.SendUpdate(_message);
+            });
+        }
+
+        private void AddAllServices(IUpdateBuilder updateBuilder)
+        {
+            PCClient.SetLoggingService(_loggingService);
+
+            updateBuilder.AddUpdatableService(new UpdatableClock());
+
+            updateBuilder.AddUpdatableService(new UpdatableCoreTempCPUMonitorService(_loggingService));
+
+            var gpuStatsService = new UpdatableGPUzMonitorService(_loggingService);
+            updateBuilder.AddUpdatableService(gpuStatsService);
+            updateBuilder.AddStaticDataService(gpuStatsService);
+
+            var audioSwitcher = new AudioSwitcherService(_loggingService);
+            updateBuilder.AddUpdatableService(audioSwitcher);
+            updateBuilder.AddStaticDataService(audioSwitcher);
+
+            updateBuilder.AddStaticDataService(new WindowsMemoryCheckService());
+
+            updateBuilder.StartAllServices();
+        }
+
+        private void ShowLoggedMessageEvent(object sender, MessageLoggedEventArgs a)
+        {
+            if (a.MessageLevel == MessageLevel.Error)
+            {
+                LoggedError = a.LoggedMessage;
+                return;
+            }
+
+            if (a.MessageLevel == MessageLevel.Warning)
+            {
+                LoggedWarning = a.LoggedMessage;
+                return;
+            }
+
+            LoggedMessage = a.LoggedMessage;
+        }
+
+    }
+}
